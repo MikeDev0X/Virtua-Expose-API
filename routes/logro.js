74458f90e31a2f2e1cc4f9dccc6075930545c911
MikeDev0X <mike0vortex@gmail.com>
2022-03-15 14:33:53 -0600

    updated middleware in logro, in order to test without needing token

--- routes/logro.js
+++ routes/logro.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const logroController = require('../controller/logro.controller');
-const middleware = require('../middleware/jwt-middleware');
+//const middleware = require('../middleware/jwt-middleware');
+const middleware = express.Router();
+
 
 router.get('/logros', middleware, logroController.getLogros);
 router.get('/logro/:id', middleware, logroController.getLogroById);
