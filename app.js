8fbef54135bdca1f05a23af95ce2ac78fef9e8fd
MikeDev0X <mike0vortex@gmail.com>
2022-05-02 11:18:02 -0500

    port modified

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const router = express.Router();
 ////////////////////////
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 ////////////////7
 app.use(cors());
